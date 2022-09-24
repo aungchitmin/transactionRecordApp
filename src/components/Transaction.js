74513f865ef6e1609ec6f426cfb2089d533ff273
Sea Lion <aungchitmin1112000@gmail.com>
2022-09-24 10:25:15 +0630

    usecontext practice

---
+++ src/components/Transaction.js
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
+const Transaction = ({ item }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
+
+  const sign = item.amount < 0 ? "-" : "+";
+  return (
+    <li className={item.amount < 0 ? "minus" : "plus"}>
+      {item.text}{" "}
+      <span>
+        {sign}${Math.abs(item.amount)}
+      </span>
+      <button className="delete-btn" onClick={() => deleteTransaction(item.id)}>
+        x
+      </button>
+    </li>
+  );
+};
+
+export default Transaction;
